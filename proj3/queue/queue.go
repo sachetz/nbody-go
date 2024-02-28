4a400a9eb6d7b9753f96d8a009f456d50a31408e
sachetz <zodesachet@gmail.com>
2024-02-28 02:43:28 -0600

    Updated queue to use atomic int

--- proj3/queue/queue.go
+++ proj3/queue/queue.go
@@ -1,36 +1,40 @@
 package queue
 
+import "sync/atomic"
+
 type BoundedDeque struct {
 	tasks  []func()
-	bottom int32
+	bottom atomic.Int32
 	top    *AtomicStampedReference
 }
+
+// TODO - convert lock based implementation to atomic implementation
 
 func NewBDEQueue(capacity int) *BoundedDeque {
 	queue := &BoundedDeque{}
 	queue.tasks = make([]func(), capacity)
-	queue.bottom = 0
+	queue.bottom.Store(0)
 	queue.top = &AtomicStampedReference{}
 	return queue
 }
 
+// PushBottom is only used by the owner thread - concurrency not required
 func (q *BoundedDeque) PushBottom(task func()) {
-	q.tasks[q.bottom] = task
-	q.bottom += 1
+	q.tasks[q.bottom.Load()] = task
+	q.bottom.Add(1)
 }
 
-func (q *BoundedDeque) IsEmpty() bool {
-	return q.top.GetReference() < q.bottom
-}
-
+// Try to atomically pop from the top and increment the top reference and stamp
 func (q *BoundedDeque) PopTop() func() {
 	var oldStamp int32
 	oldTop := q.top.Get(&oldStamp)
 	newTop := oldTop + 1
 	newStamp := oldStamp + 1
-	if q.bottom <= oldTop {
+	// Queue is empty, nothing to pop
+	if q.bottom.Load() <= oldTop {
 		return nil
 	}
+	// Try to atomically update the top reference and the stamp
 	f := q.tasks[oldTop]
 	if q.top.CompareAndSet(oldTop, newTop, oldStamp, newStamp) {
 		return f
@@ -39,24 +43,31 @@ func (q *BoundedDeque) PopTop() func() {
 }
 
 func (q *BoundedDeque) PopBottom() func() {
-	if q.bottom == 0 {
+	if q.bottom.Load() == 0 { // If queue is empty, return nil
 		return nil
 	}
-	q.bottom -= 1
-	f := q.tasks[q.bottom]
+
+	// Otherwise decrement bottom and claim a task
+	q.bottom.Add(-1)
+	f := q.tasks[q.bottom.Load()]
+
 	var oldStamp int32
 	oldTop := q.top.Get(&oldStamp)
 	var newTop int32 = 0
 	newStamp := oldStamp + 1
-	if q.bottom > oldTop {
+
+	// Return the function - bottom is not equal to top and concurrency not required
+	if q.bottom.Load() > oldTop {
 		return f
 	}
-	if q.bottom == oldTop {
-		q.bottom = 0
+	// Bottom = top, atomically update the top reference and stamp - other threads might be interacting
+	if q.bottom.Load() == oldTop {
+		q.bottom.Store(0)
 		if q.top.CompareAndSet(oldTop, newTop, oldStamp, newStamp) {
 			return f
 		}
 	}
+	// Failed to fetch, i.e. thief succeed but top not updated, update and return
 	q.top.Set(newTop, newStamp)
 	return nil
 }
